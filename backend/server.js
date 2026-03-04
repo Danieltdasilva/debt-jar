0b539b3787693435dfb0dee10ee2f0d814991bae
Daniel Silva <danieltdasilva@outlook.com>
2026-03-04 13:57:17 -0500

    stashing away the modules

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/debttracker")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
